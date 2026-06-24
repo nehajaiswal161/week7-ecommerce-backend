3ec2599d86fd298ee50a36ffd2a96a50d2083208
nehajaiswal161 <nehajaiswal0602@gmail.com>
2026-06-24 18:25:11 +0530

    Create Payment.java

---
+++ src/main/java/com/ecommerce/model/entity/Payment.java
@@ -0,0 +1,12 @@
+package com.ecommerce.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
