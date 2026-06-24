88c1ddd6d152d0c7011977ca57cfde6de4ad4664
nehajaiswal161 <nehajaiswal0602@gmail.com>
2026-06-24 18:23:04 +0530

    Create Product.java

---
+++ src/main/java/com/ecommerce/model/entity/Product.java
@@ -0,0 +1,26 @@
+package com.ecommerce.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Table(name="products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    private Integer stock;
+
+    @ManyToOne
+    private Category category;
+}
