bc89d476f6a48b417c0ef2244507800e413dae33
nehajaiswal161 <nehajaiswal0602@gmail.com>
2026-06-24 18:21:59 +0530

    Create PaymentRepository.java

---
+++ src/main/java/com/ecommerce/repository/PaymentRepository.java
@@ -0,0 +1,8 @@
+package com.ecommerce.repository;
+import com.ecommerce.model.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
