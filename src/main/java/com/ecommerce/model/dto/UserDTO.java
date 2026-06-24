0f60fd482f15b376f3874443c66fd7410189bc94
nehajaiswal161 <nehajaiswal0602@gmail.com>
2026-06-24 18:46:19 +0530

    Create UserDTO.java

---
+++ src/main/java/com/ecommerce/model/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.ecommerce.model.dto;
+
+public class UserDTO {
+    private String username;
+    private String email;
+
+    public UserDTO() {}
+
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
