f37bb53ab20ba30ba92e17d4aacb3a9baf84d735
nehajaiswal161 <nehajaiswal0602@gmail.com>
2026-06-24 18:45:00 +0530

    Update ProductDTO.java

--- src/main/java/com/ecommerce/model/dto/ProductDTO.java
+++ src/main/java/com/ecommerce/model/dto/ProductDTO.java
@@ -1,1 +1,17 @@
+package com.ecommerce.model.dto;
 
+public class ProductDTO {
+    private String name;
+    private Double price;
+    private Integer stockQuantity;
+
+    public ProductDTO() {}
+
+  
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Double getPrice() { return price; }
+    public void setPrice(Double price) { this.price = price; }
+    public Integer getStockQuantity() { return stockQuantity; }
+    public void setStockQuantity(Integer stockQuantity) { this.stockQuantity = stockQuantity; }
+}
