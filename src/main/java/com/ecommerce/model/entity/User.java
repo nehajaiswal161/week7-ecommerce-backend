35e700572b7f2f14520a9fbd45b25177ad2d8153
nehajaiswal161 <nehajaiswal0602@gmail.com>
2026-06-24 18:22:35 +0530

    Create User.java

---
+++ src/main/java/com/ecommerce/model/entity/User.java
@@ -0,0 +1,29 @@
+package com.ecommerce.model.entity;
+
+import com.ecommerce.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
